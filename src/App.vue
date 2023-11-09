e55c788bb697a138acb3c20399a5f13af979eb74
nayzawaung750nza <nayzawaung750@gmail.com>
2023-11-09 15:38:22 +0630

    Methods

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,27 @@
 <template>
-   
-      <div>
-        <h2 v-for="(name, index) in names" :key="name" > {{ index }} {{ name }}</h2>
-        <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
-        <div v-for="actor in actors" :key="actor.name">
-            <h2>{{ actor.name }}</h2>
-            <p v-for="movie in actor.movies" :key="movie">{{ movie }} </p>
-        </div>
-        <h6 v-for="value in myInfo" :key="value">{{ value }}</h6>
-        <h6 v-for="(value, key, index) in myInfo" :key="value">{{ index }} {{ key }} {{ value }}</h6>
-      </div>
-      <template v-for="name in names" :key="name">
-        <h2>{{ name }}</h2>
-        <input placeholder="Last name" />
-        <hr>
-      </template>
-      <button @click="shuffle">shuffle!</button>
-
-      <template v-for="name in names" :key="name">
-        <h3 v-if="name === 'Aung Aung'">{{ name }}</h3>
-      </template>
-      
+   <div>
+    <h2>Add method - {{ add(10,2,34) }}</h2>
+    <h2>Multiply method - {{ multiply(5) }}</h2>
+    <h2>Multiply method - {{ multiply(baseValue) }}</h2>
+   </div>
 </template>
 
 <script>
-import _ from 'lodash';
 export default {
   name: 'App',
   data(){
     return{
-      names:['Nay zaw','Aung Aung','Maung Maung'],
-      fullNames:[
-        {first:'Nay', last:'Zaw'},
-        {first:'Zaw', last:'Gyi'},
-        {first:'Aung', last:'Min'},
-        {first:'Loon', last:'Naw'},
-        {first:'May', last:'Mie'},
-    ],
-    actors:[
-      {
-        name:'Nay Toe',
-        movies:['Mone Swal','Kyauk Kyauk Kyauk']
-      },
-      {
-        name:'Zan Kyi',
-        movies:['Now & Ever','Mudras Calling']
-      },
-      {
-        name:'Pyi Ti Oo',
-        movies:['Red Cotton Silk Flower','Toe Kyaw Man Nae Nan San Tae Ywar']
-      }
-    ],
-    myInfo:{//object
-        name:'Nay Zaw',
-        channel:'Developer NAY',
-        course:'Vue JS',
+      basemultipleNumber:5,
+      baseValue:6,
     }
-  }
-},
-methods:{
-  shuffle(){
-    console.log(this.names);
-    this.names = _.shuffle(this.names);
   },
+methods:{
+    add(a,b,c){
+      return a+b+c
+    },
+    multiply(num){
+      return num*this.basemultipleNumber
+    }
 },
 }
 </script>

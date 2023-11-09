20894232aaa835d7a3898c26564ba6e1dbd3c4d6
nayzawaung750nza <nayzawaung750@gmail.com>
2023-11-09 16:52:12 +0630

    Form Handling (Part 2)

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <pre>
       {{ JSON.stringify(formValues, null, 2) }}
     </pre>
-    <form>
+    <form @submit="submitForm">
       <div class="rendered-form">
         <div class="formbuilder-text form-group field-name">
             <label for="name" class="formbuilder-text-label">Name</label>
@@ -40,6 +40,42 @@
     </div>
 </div>
 
+<div class="rendered-form">
+    <div class="formbuilder-checkbox-group form-group field-remote-work">
+        <div class="checkbox-group">
+            <div class="formbuilder-checkbox">
+                <input name="remote-work[]" true-value="yes" false-value="no" v-model="formValues.remoteWork" id="remote-work-0" value="open-to-remote-work" type="checkbox" checked="checked">
+                <label for="remote-work-0">Opent to remote work!</label>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+  <label for="html">HTML</label>
+  <input type="checkbox"  value="html" id="html" v-model="formValues.skillSet">
+  <label for="js">JS</label>
+  <input type="checkbox"  value="js" id="js" v-model="formValues.skillSet">
+  <label for="php">PHP</label>
+  <input type="checkbox"  value="php" id="php" v-model="formValues.skillSet">
+  <label for="vue">Vue</label>
+  <input type="checkbox"  value="vue" id="vue" v-model="formValues.skillSet">
+</div>
+
+<div>
+  <label >Experience</label>
+  <input type="radio"  value="0-2" id="0-2" v-model="formValues.yearOfExperience">
+  <label for="0-2">0 - 2</label>
+  <input type="radio"  value="2-5" id="2-5" v-model="formValues.yearOfExperience">
+  <label for="2-5">2 - 5</label>
+  <input type="radio"  value="5-10" id="5-10" v-model="formValues.yearOfExperience">
+  <label for="5-10">5 - 10</label>
+  <input type="radio"  value="10+" id="10+" v-model="formValues.yearOfExperience">
+  <label for="10+">10+</label>
+</div>
+<div>
+  <button>Submit</button>
+</div>
+
     </form>
    </div>
 </template>
@@ -54,11 +90,18 @@ export default {
         profileSummeery:'',
         country:'',
         jobLocation:[],
+        remoteWork:'no',
+        skillSet:[],
+        yearOfExperience:'',
       }
     }
   },
 methods:{
-  
+  submitForm(event){
+    event.preventDefault()
+    console.log('Form Value ' , this.formValues)
+    
+  }
 },
 }
 </script>

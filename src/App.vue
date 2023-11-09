672190a4519e1f8ed57763cdfb5c7342a0b53501
nayzawaung750nza <nayzawaung750@gmail.com>
2023-11-09 11:39:47 +0630

    binding class

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+     <h1>{{ greet }} {{ name }}</h1> <!-- Text Binding -->
+     <p v-html="channel" ></p><!-- Binding HTML -->
+     <div v-html="hack"></div>
+     <h3 v-bind:id="headingID">Heading</h3> <!-- Attribute Binding -->
+     <button v-bind:disabled="isDisabled">Bind</button>
+     <h4 class="underline" v-bind:class="status">Underline</h4>
+     <h4 v-bind:class="isPromoted && 'promoted'">Promoted Movies</h4>
+     <h5 v-bind:class="isSoldOut ? 'sold-out':'new'">isSoldOut? movies</h5>
+     <h5 v-bind:class="['new', 'promoted']">Newly Promoted Movies</h5>
+     <h5 v-bind:class="[isPromoted&& 'promoted', isSoldOut?'sold-out':'new']">Array conditional movies</h5>
+    <h5 v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut,
+    }">Object conditional movies</h5>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      greet:'Hello',
+      name:'Nay Zaw',
+      channel: '<b>Developer Nay</b>',
+      hack: `<a href="#" onclick="alert('hacks')"> Win a Prize</a>`,
+      headingID : 'heading',
+      isDisabled: true,
+      status:'success',
+      isPromoted:true,
+      isSoldOut:true,
+    }
   }
+  
 }
 </script>
 
@@ -22,5 +48,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.promoted{
+  font-style: italic;
+}
+.success{
+  color: green;
+}
+.underline{
+  text-decoration: underline;
+}
+.new{
+  color:olivedrab
+}
+.sold-out{
+  color : red;
 }
 </style>

741588b788f86baae7ce9d89d2354a6f43847688
nayzawaung750nza <nayzawaung750@gmail.com>
2023-11-10 10:24:46 +0630

    Immediate and Deep Watchers

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,18 @@
       <button @click="volume += 2">Increase</button>
       <button @click="volume -= 2">Decrease</button>
     </div>
+    <input type="text" v-model="movie" name="" id="">
+
+    <div>
+      <input type="text" v-model="movieInfo.title" name="" id="">
+
+    <input type="text" v-model="movieInfo.actor" name="" id="">
+    </div>
+    <div>
+      <button @click="movieList.push('Wonder Woman')">Add Movie</button>
+
+      <button @click="movieList = movieList.concat(['Wonder Woman'])">Add Movie</button>
+    </div>
    </div>
 
    
@@ -16,7 +28,13 @@ export default {
   name: 'App',
   data(){
     return{
-      volume:0
+      volume:0,
+      movie:'Batman',
+      movieInfo:{
+        title:'',
+        actor:'',
+      },
+      movieList:['Batman','Superman','Antman'],
     }
   },
 methods:{},
@@ -26,7 +44,26 @@ watch:{
       if(newVolume> oldVolume && newVolume === 16){
         alert('Listening to a high volume for a long time may damage your hearing')
       }
+  },
+  movie:{
+      handler(newMovie){
+      console.log(`calling api - ${newMovie}`);
+    },
+    immediate:true,
+  },
+  movieInfo:{
+    handler(newMovie){
+      console.log(`Calling api = ${newMovie.title} and ${newMovie.actor}`);
+    },
+    deep:true,
+  },
+  movieList:{
+    handler(newMovie){
+      console.log(`Update movie list ${newMovie}`);
+    },
+    //deep:true, //push () using deep:true, concat() not using deep:true
   }
+  
 },
 }
 </script>
